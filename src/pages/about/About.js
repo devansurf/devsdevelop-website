603d57ce324af4eae5759b89149c4bbfda37e951
devansurf <devan.vazquez@upr.edu>
2024-06-03 14:22:23 -0400

    added version for pocketsurf

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -2,10 +2,17 @@ import AboutMe from "../../components/AboutMe";
 import PageHeader from "../../components/PageHeader";
 
 const About = ({ name, location, brand, email, availability }) => {
+  const appVersion = "2.1.0"; 
   return (
     <section className="about">
       <PageHeader title="About Me" description="Let me introduce myself" />
       <AboutMe name={name} location={location} brand={brand} email={email} availability={availability} />
+      <div id="app-version-playstore" style={{ display: 'none' }}>
+        {appVersion}
+      </div>
+      <div id="app-version-appstore" style={{ display: 'none' }}>
+        {appVersion}
+      </div>
     </section>
   );
 };
